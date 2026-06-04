60c4ce9998bbf8d44939807131d3fbf04d4e1cf6
Damien Lallemand <damienlallemand54@gmail.com>
2026-06-04 13:36:05 +0200

    mise à jour Program.cs + db et migrations

---
+++ src/Microservices/PatientService/Data/PatientDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientService.Entities;
+
+namespace PatientService.Data
+{
+    public class PatientDbContext : DbContext
+    {   
+        public PatientDbContext(DbContextOptions<PatientDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Patient> Patients { get; set; } = null!;
+    }
+}
