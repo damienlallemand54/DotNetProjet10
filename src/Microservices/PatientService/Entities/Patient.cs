f12f0f80f64552c569962f48725c1fae53bc3e01
Damien Lallemand <damienlallemand54@gmail.com>
2026-06-04 13:21:12 +0200

    Ajout des couches et de Patient.cs

---
+++ src/Microservices/PatientService/Entities/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace PatientService.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public required string Gender { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
